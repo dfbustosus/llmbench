1910e8a665279590637c2f446ceceb7a974af6a9
David BU <dfbustos@uninorte.edu.co>
2026-03-15 11:15:28 -0300

    Update semantic index

--- packages/core/src/scorers/semantic/index.ts
+++ packages/core/src/scorers/semantic/index.ts
@@ -1,1 +1,7 @@
+export { BleuScorer } from "./bleu.js";
 export { CosineSimilarityScorer } from "./cosine-similarity.js";
+export type { EmbedFn } from "./embedding-similarity.js";
+export { EmbeddingSimilarityScorer } from "./embedding-similarity.js";
+export { LevenshteinScorer } from "./levenshtein.js";
+export { RougeScorer } from "./rouge.js";
+export { countNgrams, getNgrams, tokenize } from "./tokenizer.js";
