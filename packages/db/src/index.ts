82da903858727761c58e381c5603cc7ba9101d25
David BU <dfbustos@uninorte.edu.co>
2026-03-21 12:58:07 -0300

    feat: export DEFAULT_LIMITS and BATCH_CHUNK_SIZE from db package

--- packages/db/src/index.ts
+++ packages/db/src/index.ts
@@ -1,4 +1,5 @@
 export type { LLMBenchDB } from "./client.js";
 export { createDB, createInMemoryDB, initializeDB } from "./client.js";
+export { BATCH_CHUNK_SIZE, DEFAULT_LIMITS } from "./constants.js";
 export * from "./repositories/index.js";
 export * from "./schema/index.js";
