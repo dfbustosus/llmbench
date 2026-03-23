644ef4675f6ef835c8145a81fb26ee2db27fa97a
David BU <dfbustos@uninorte.edu.co>
2026-03-22 21:10:32 -0300

    feat: add streaming parsers barrel export

---
+++ packages/core/src/providers/streaming/index.ts
@@ -0,0 +1,5 @@
+export type { BedrockEvent } from "./bedrock-event-stream-parser.js";
+export { parseBedrockEventStream } from "./bedrock-event-stream-parser.js";
+export { parseNDJSON } from "./ndjson-parser.js";
+export type { SSEEvent } from "./sse-parser.js";
+export { parseSSE } from "./sse-parser.js";
