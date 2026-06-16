71de15007eddc880c57b35ee27a955eacbc23d54
David BU <dfbustos@uninorte.edu.co>
2026-06-16 19:56:26 -0400

    style(ui): sort card exports

--- packages/ui/src/components/card.tsx
+++ packages/ui/src/components/card.tsx
@@ -44,4 +44,4 @@ const CardContent = React.forwardRef<HTMLDivElement, React.HTMLAttributes<HTMLDi
 );
 CardContent.displayName = "CardContent";
 
-export { Card, CardHeader, CardTitle, CardDescription, CardContent };
+export { Card, CardContent, CardDescription, CardHeader, CardTitle };
